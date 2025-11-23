873d5e10f056848323da463eddae74bde1364ebc
Aysha <ayshaalmatrooshi703@gmail.com>
2025-11-23 16:20:41 +0400

    Implement main program for BASIC COMPUTER SIMULATOR
    Implemented main.py which initializes the Basic Computer components and provides a mode selection menu for launching either the CLI
    or GUI interface.

---
+++ main.py
@@ -0,0 +1,56 @@
+# main.py
+
+from simulator.machine import BasicComputer
+from profiler.profiler import Profiler
+from utils.formatter import Formatter
+
+from cli.interface import CommandLineInterface
+
+
+try:
+    from gui.app import start_gui
+    GUI_AVAILABLE = True
+except ImportError:
+    GUI_AVAILABLE = False
+
+
+def start_cli_mode(machine, profiler, formatter):
+    print("\n CLI mode...\n")
+    cli = CommandLineInterface(machine, profiler, formatter)
+    cli.start()
+
+
+def start_gui_mode(machine):
+    if not GUI_AVAILABLE:
+        print("GUI is not available in this project.")
+        return
+    print("\n GUI mode...\n")
+    start_gui(machine)
+
+
+def main():
+    print("       BASIC COMPUTER SIMULATOR")
+    print("Choose mode:")
+    print("1. Command Line Interface (CLI)")
+    print("2. Graphical User Interface (GUI)")
+    print("3. Exit\n")
+
+    choice = input("> ").strip()
+
+    # Build core components
+    machine = BasicComputer()
+    profiler = Profiler(machine)
+    formatter = Formatter()
+
+    if choice == "1":
+        start_cli_mode(machine, profiler, formatter)
+
+    elif choice == "2":
+        start_gui_mode(machine)
+
+    else:
+        print("Exiting.")
+
+
+if __name__ == "__main__":
+    main()
